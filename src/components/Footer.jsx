3db42f078c234fb73cb938a554be4161330ef5eb
emman <emmanuelablao16@gmail.com>
2025-05-03 22:04:30 +0800

    fixing padding in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-900 text-white py-12 pb-21">
+    <footer className="bg-gray-900 text-white py-21 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* Logo and description */}
